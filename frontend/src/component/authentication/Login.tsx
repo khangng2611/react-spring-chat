4dbb72424ef1e0073c9584927d0d52b692079694
Khang Nguyen Phuc <khangnguyen261102@gmail.com>
2024-06-27 20:24:35 +0700

    update: [FE] UI for login component

--- frontend/src/component/authentication/Login.tsx
+++ frontend/src/component/authentication/Login.tsx
@@ -1,5 +1,6 @@
 import React, { useCallback, useState } from 'react';
 import { useSession } from '../../context/SessionContext';
+import { TONE_COLORS } from '../../constant/color';
 
 const Login = () => {
     const [userInfo, setUserInfo] = useState({
@@ -24,51 +25,58 @@ const Login = () => {
 
     return (
         <div className="flex flex-col items-center justify-center h-screen">
-            <h2 className="text-2xl font-bold mb-4">Login</h2>
-            <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-                <div className="mb-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
-                        Full Name:
-                    </label>
-                    <input
-                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        name="fullName"
-                        type="text"
-                        value={userInfo.fullName}
-                        minLength={6}
-                        maxLength={100}
-                        onChange={handleFormChange}
-                    />
-                </div>
-                <div className="mb-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+            <h2
+                className="text-4xl font-bold mb-8"
+                style={{color: TONE_COLORS.PRIMARY}}
+            >
+                Join ChatterBox !
+            </h2>
+            <form onSubmit={handleSubmit} className="flex flex-col bg-blue-200 shadow-md rounded px-12 pt-10 pb-12 mb-8 w-96">
+                <div className="mb-6">
+                    <label className="block text-gray-700 text-lg font-bold mb-4" htmlFor="email">
                         Username:
                     </label>
                     <input
-                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        className="shadow appearance-none border rounded w-full py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                         name="username"
                         type="text"
                         value={userInfo.username}
+                        placeholder='Enter your username'
                         minLength={6}
                         maxLength={100}
                         onChange={handleFormChange}
                     />
                 </div>
-                <div className="mb-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                <div className="mb-6">
+                    <label className="block text-gray-700 text-lg font-bold mb-4" htmlFor="email">
+                        Full name:
+                    </label>
+                    <input
+                        className="shadow appearance-none border rounded w-full py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        name="fullName"
+                        type="text"
+                        value={userInfo.fullName}
+                        placeholder='Enter your full name'
+                        minLength={6}
+                        maxLength={100}
+                        onChange={handleFormChange}
+                    />
+                </div>
+                {/* <div className="mb-6">
+                    <label className="block text-gray-700 text-lg font-bold mb-4" htmlFor="password">
                         Password:
                     </label>
                     <input
-                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        className="shadow appearance-none border rounded w-full py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                         name="password"
                         type="password"
                         value={userInfo.password}
                         minLength={6}
                         onChange={handleFormChange}
                     />
-                </div>
+                </div> */}
                 <button
-                    className="bg-sky-600 hover:bg-sky-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                    className="bg-sky-600 hover:bg-sky-700 text-white font-bold py-3 px-6 rounded focus:outline-none focus:shadow-outline"
                     type="submit"
                 >
                     Login
